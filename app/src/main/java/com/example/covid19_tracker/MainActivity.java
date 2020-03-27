8ceafccfb74ece684b02657ea8c95ff9b0ab034c
Trent <suzukitrent@gmail.com>
2020-03-26 22:10:52 -0700

    Added simple text field and button

--- app/src/main/java/com/example/covid19_tracker/MainActivity.java
+++ app/src/main/java/com/example/covid19_tracker/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.covid19_tracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +12,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /** Called when user taps the send button */
+    public void sendMessage(View view) {
+        //Do something
+    }
+
 }
