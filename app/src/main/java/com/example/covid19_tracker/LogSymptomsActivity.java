380813d4b8c4c9bb6df4f1e8cc6a94b27174a17b
Trent <suzukitrent@gmail.com>
2020-03-28 13:46:58 -0700

    Added main page buttons

---
+++ app/src/main/java/com/example/covid19_tracker/LogSymptomsActivity.java
@@ -0,0 +1,16 @@
+package com.example.covid19_tracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class LogSymptomsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_symptoms);
+    }
+
+}
--- app/src/main/java/com/example/covid19_tracker/MainActivity.java
+++ app/src/main/java/com/example/covid19_tracker/MainActivity.java
@@ -16,11 +16,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /** Called when user taps the send button */
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.editText);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
+    public void logSymptoms(View view) {
+        Intent intent = new Intent(this, LogSymptomsActivity.class);
+//        EditText editText = (EditText) findViewById(R.id.editText);
+//        String message = editText.getText().toString();
+//        intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
     }
 
