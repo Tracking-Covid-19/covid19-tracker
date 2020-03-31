ace8f16ab0ce94e8c267cdd25bd38e81103cf2f4
Trent <suzukitrent@gmail.com>
2020-03-31 10:25:11 -0700

    Created classes for main functions

--- app/src/main/java/com/example/covid19_tracker/FindPeopleActivity.java
+++ app/src/main/java/com/example/covid19_tracker/FindPeopleActivity.java
@@ -1,14 +1,14 @@
 package com.example.covid19_tracker;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class LogSymptomsActivity extends AppCompatActivity {
+public class FindPeopleActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_log_symptoms);
+        setContentView(R.layout.activity_find_people);
     }
 
 }
