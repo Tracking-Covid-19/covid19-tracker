fe72a836bd5b8e4f601c68f5e0fc5896ecc63a6b
Ivan-Bao <ivanbao2048@gmail.com>
2020-04-09 20:44:41 -0700

    Added date display at log symptom page

--- app/src/main/java/com/example/covid19_tracker/LogSymptomsActivity.java
+++ app/src/main/java/com/example/covid19_tracker/LogSymptomsActivity.java
@@ -3,6 +3,11 @@ package com.example.covid19_tracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class LogSymptomsActivity extends AppCompatActivity {
 
@@ -10,6 +15,17 @@ public class LogSymptomsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_symptoms);
+
+        //create a date string.
+        String date_n = new SimpleDateFormat("MMM dd, yyyy", Locale.getDefault()).format(new Date());
+        //get hold of textview.
+        TextView date  = findViewById(R.id.dateText);
+        //set it as current date.
+        date.setText(date_n);
     }
+
+
+
+
 
 }
