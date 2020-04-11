d90c4f03c8c04322e8eb29142e68c0824e946cf3
Ivan-Bao <ivanbao2048@gmail.com>
2020-04-11 13:12:33 -0700

    added log symptom log functionality

--- app/src/main/java/com/example/covid19_tracker/LogSymptomsActivity.java
+++ app/src/main/java/com/example/covid19_tracker/LogSymptomsActivity.java
@@ -3,8 +3,14 @@ package com.example.covid19_tracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
 import android.widget.TextView;
-
+import android.widget.Toast;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
@@ -24,8 +30,52 @@ public class LogSymptomsActivity extends AppCompatActivity {
         date.setText(date_n);
     }
 
+    Symptoms readSeekBars(){
+        SeekBar coughBar = findViewById(R.id.seekBar1);
+        SeekBar sniffleBar = findViewById(R.id.seekBar2);
+        SeekBar soreBar = findViewById(R.id.seekBar3);
+        SeekBar muscleBar = findViewById(R.id.seekBar4);
+        SeekBar feverBar = findViewById(R.id.seekBar5);
+        SeekBar breathBar = findViewById(R.id.seekBar6);
 
+        Symptoms todaySymptom = new Symptoms(coughBar.getProgress(), sniffleBar.getProgress(), soreBar.getProgress(),
+                muscleBar.getProgress(), feverBar.getProgress(), breathBar.getProgress());
+        return todaySymptom;
+    }
 
+    void saveRecords (Symptoms symptom){
+        FileOutputStream fos = null;
+        try {
+            StringBuilder symptomString = new StringBuilder();
+            fos = openFileOutput("symptomLog.txt", MODE_APPEND);
+            symptomString.append(symptom.sniffles + " ");
+            symptomString.append(symptom.muscleAches + " ");
+            symptomString.append(symptom.cough + " ");
+            symptomString.append(symptom.soreThroat + " ");
+            symptomString.append(symptom.fever + " ");
+            symptomString.append(symptom.difficultyBreathing + " ");
+            symptomString.append(symptom.date);
+            symptomString.append("\n");
+            String toWrite = symptomString.toString();
+            fos.write(toWrite.getBytes());
+            Toast.makeText(this, "Saved to " + getFilesDir() + "/symptomLog.txt", Toast.LENGTH_LONG);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (fos != null){
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+        }
+    }
+//    final Button submitButton = findViewById(R.id.textView6);
+    public void submitLog(View view){
+        saveRecords(readSeekBars());
+    }
 
 
 }
--- app/src/main/java/com/example/covid19_tracker/Symptoms.java
+++ app/src/main/java/com/example/covid19_tracker/Symptoms.java
@@ -1,11 +1,29 @@
 package com.example.covid19_tracker;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class Symptoms {
-    public final int cough = 0;
-    public final int sniffles = 0;
-    public final int soreThroat = 0;
-    public final int MuscleAches = 0;
+    public final int cough;
+    public final int sniffles;
+    public final int soreThroat;
+    public final int muscleAches;
+    public final int fever;
+    public final int difficultyBreathing;
+    String date;
 
-
+    Symptoms(int cough, int sniffles, int soreThroat, int muscleAches, int fever, int difficultyBreathing){
+        if (cough > 10 || cough < 0 || sniffles > 10 || sniffles < 0 || soreThroat > 10 || soreThroat < 0 ||
+                muscleAches > 10 || muscleAches < 0 || fever > 10 || fever < 0 || difficultyBreathing > 10 || difficultyBreathing < 0){
+            throw new IllegalArgumentException("symptom level out of bound");
+        }
+        this.cough = cough;
+        this.sniffles = sniffles;
+        this.soreThroat = soreThroat;
+        this.muscleAches = muscleAches;
+        this.fever = fever;
+        this.difficultyBreathing = difficultyBreathing;
+        date = new SimpleDateFormat("MMM dd, yyyy", Locale.getDefault()).format(new Date());
+    }
 
 }
