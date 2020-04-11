db727138b11651331045488d533c59cfd6d20c3b
Ivan-Bao <ivanbao2048@gmail.com>
2020-04-10 17:11:56 -0700

    added difficulty breathing and fever option at symptom logs

---
+++ app/src/main/java/com/example/covid19_tracker/Symptoms.java
@@ -0,0 +1,11 @@
+package com.example.covid19_tracker;
+
+public class Symptoms {
+    public final int cough = 0;
+    public final int sniffles = 0;
+    public final int soreThroat = 0;
+    public final int MuscleAches = 0;
+
+
+
+}
