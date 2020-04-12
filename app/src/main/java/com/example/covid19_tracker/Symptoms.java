cca39d9afdc9ba32926707093c356e24fb8712a1
zsayem30 <sayemnazmuz@gmail.com>
2020-04-11 22:31:33 -0700

    symptoms class updated

--- app/src/main/java/com/example/covid19_tracker/Symptoms.java
+++ app/src/main/java/com/example/covid19_tracker/Symptoms.java
@@ -1,29 +1,31 @@
 package com.example.covid19_tracker;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
+import java.util.GregorianCalendar;
 
 public class Symptoms {
-    public final int cough;
-    public final int sniffles;
-    public final int soreThroat;
-    public final int muscleAches;
-    public final int fever;
-    public final int difficultyBreathing;
-    String date;
 
-    Symptoms(int cough, int sniffles, int soreThroat, int muscleAches, int fever, int difficultyBreathing){
-        if (cough > 10 || cough < 0 || sniffles > 10 || sniffles < 0 || soreThroat > 10 || soreThroat < 0 ||
-                muscleAches > 10 || muscleAches < 0 || fever > 10 || fever < 0 || difficultyBreathing > 10 || difficultyBreathing < 0){
-            throw new IllegalArgumentException("symptom level out of bound");
+    private static String name;
+    private int intensity;
+    private static GregorianCalendar date;
+    private String additionalDetails;
+
+    Symptoms (String name, String additionalDetails, int intensity) {
+        this.intensity = intensity;
+        if (intensity > 10 || intensity < 0){
+            throw new IllegalArgumentException("Please enter an intensity between 0 and 10.");
         }
-        this.cough = cough;
-        this.sniffles = sniffles;
-        this.soreThroat = soreThroat;
-        this.muscleAches = muscleAches;
-        this.fever = fever;
-        this.difficultyBreathing = difficultyBreathing;
-        date = new SimpleDateFormat("MMM dd, yyyy", Locale.getDefault()).format(new Date());
+        this.name = name;
+        this.additionalDetails = additionalDetails;
     }
 
+    Symptoms (String name, int intensity) {
+        this.intensity = intensity;
+        this.name = name;
+    }
+    public static GregorianCalendar getDate() {
+        GregorianCalendar symptomDate = date;
+        return symptomDate;
+    }
+    public static String getName() {
+        return name;
+    }
 }
