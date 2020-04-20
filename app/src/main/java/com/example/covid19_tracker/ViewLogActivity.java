5f0691e65353e9c5e7fe8869f716abdc0d4c9b10
Ivan-Bao <ivanbao2048@gmail.com>
2020-04-19 20:44:24 -0700

    view log implementation in progress. I was trying to make it a clickable list view, but still the app crashes when I click on Edit/view log. If you guys have free time feel free to look into it

--- app/src/main/java/com/example/covid19_tracker/ViewLogActivity.java
+++ app/src/main/java/com/example/covid19_tracker/ViewLogActivity.java
@@ -1,14 +1,59 @@
 package com.example.covid19_tracker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class ViewLogActivity extends AppCompatActivity {
 
+    private String[] myDataset = {"test data 1", "test data 2", "test data 3"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_log);
+        final ListView listView = findViewById(R.id.logListView);
+        final ArrayList<String> list = new ArrayList<String>();
+        for (String s: myDataset){
+            list.add(s);
+        }
+        final StableArrayAdapter adapter = new StableArrayAdapter(this,
+                R.layout.item_simple_itemview, list);
+        listView.setAdapter(adapter);
+    }
+
+    private class StableArrayAdapter extends ArrayAdapter<String> {
+
+        HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
+
+        public StableArrayAdapter(Context context, int textViewResourceId,
+                                  List<String> objects) {
+            super(context, textViewResourceId, objects);
+            for (int i = 0; i < objects.size(); ++i) {
+                mIdMap.put(objects.get(i), i);
+            }
+        }
+
+        @Override
+        public long getItemId(int position) {
+            String item = getItem(position);
+            return mIdMap.get(item);
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
+
     }
 
 }
+
